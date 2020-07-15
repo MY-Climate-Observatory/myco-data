a67bf23531dc434fa85a2817b1aa1d2edca15126
XD-Ooi <43445880+XD-Ooi@users.noreply.github.com>
2020-07-15 09:02:34 +0800

    Create pollutant-freq.py

---
+++ CLIMATExScience/air-pollution-index/data-visualization/pollutant-freq.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+17 June 2020
+Author: Xiandi Ooi
+Visualizing the types of pollutants.
+ 
+"""
+
+import pandas as pd
+from plotly.offline import plot
+import plotly.graph_objects as go
+
+df = pd.read_csv(r"filepath/filename", sep = ";")
+
+# Make the selection
+selected_area = "Sandakan"
+df_select = df.loc[(df.Area == selected_area),
+                   ["Area", "Dominant", "Datetime"]]
+
+# Data wrangling for this particular visual
+df_update = df_select.set_index(pd.DatetimeIndex(df_select["Datetime"]))
+df_update.drop(df_update.columns[2], axis = 1, inplace = True)
+
+# Wrangling 
+df_group_time = df_update.groupby(pd.Grouper(freq = "Q")).size().reset_index(name = "Total")
+
+df_group = df_update.groupby([pd.Grouper(freq = "Q"),
+                              pd.Grouper("Dominant")]).size().reset_index(name = "Count")
+df_output = df_group.set_index("Datetime").join(df_group_time.set_index("Datetime"))
+df_output["Frequency"] = df_output["Count"] / df_output["Total"]
+
+# Creating df subset for the stacked bars, here we are only dealing with the main dominant pollutants
+df_pm2_5 = df_output.loc[(df_output.Dominant == "**")]
+df_pm10 = df_output.loc[(df_output.Dominant == "*")]
+df_so2 = df_output.loc[(df_output.Dominant == "a")]
+df_no2 = df_output.loc[(df_output.Dominant == "b")]
+df_o3 = df_output.loc[(df_output.Dominant == "c")]
+df_co = df_output.loc[(df_output.Dominant == "d")]
+
+# Now comes the bar chart
+fig = go.Figure()
+
+fig.add_trace(go.Bar(x = df_pm2_5.index, 
+                     y = df_pm2_5["Frequency"],
+                     name = "PM 2.5"))
+
+fig.add_trace(go.Bar(x = df_pm10.index, 
+                     y = df_pm10["Frequency"],
+                     name = "PM 10"))
+
+fig.add_trace(go.Bar(x = df_so2.index, 
+                     y = df_so2["Frequency"],
+                     name = "SO2"))
+
+fig.add_trace(go.Bar(x = df_no2.index, 
+                     y = df_no2["Frequency"],
+                     name = "NO2"))
+
+fig.add_trace(go.Bar(x = df_o3.index, 
+                     y = df_o3["Frequency"],
+                     name = "O3"))
+
+fig.add_trace(go.Bar(x = df_co.index, 
+                     y = df_co["Frequency"],
+                     name = "CO"))
+
+fig.update_layout(barmode = "stack", title_text="Frequency of Detected Pollutants")
+
+plot(fig)
