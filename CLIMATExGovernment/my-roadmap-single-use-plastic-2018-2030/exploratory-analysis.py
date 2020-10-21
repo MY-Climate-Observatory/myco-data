6d3b21009977a5c6948274a8d7ba8666552e586a
XD-Ooi <43445880+XD-Ooi@users.noreply.github.com>
2020-10-21 21:01:24 +0800

    Update exploratory-analysis.py

--- CLIMATExGovernment/my-roadmap-single-use-plastic-2018-2030/exploratory-analysis.py
+++ CLIMATExGovernment/my-roadmap-single-use-plastic-2018-2030/exploratory-analysis.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-18 Oct 2020
+21 Oct 2020
 Author: Xiandi Ooi
 
 This script is for performing exploratory analysis on the data, focusing on:
-    1. the most common words
-    2. amount of vocabulary
+    1. most common words via word clouds
+    2. number of words
+
+The following code is modified from the following source:
+    Title: 2-Exploratory-Data-Analysis, NLP in Python Tutorial
+    Author: Alice Zhao
+    Date: 2018
+    Availability: https://github.com/adashofdata/nlp-in-python-tutorial/blob/master/2-Exploratory-Data-Analysis.ipynb
 """
 import pandas as pd
 from wordcloud import WordCloud
@@ -25,8 +31,8 @@ common_df = pd.DataFrame.from_dict(common, orient = "index")
 
 # pages 0, 3, 7, 8, 9, 11, 13 has very little wordings, so we will not dive into much of it
 pages = [1,2,4,5,6,10,12,14,15]
-# plot bar chart for most common words
 
+# WORD CLOUD
 # loading the corpus for word cloud plotting
 corpus = pd.read_pickle("data_clean.pkl")
 
@@ -50,3 +56,16 @@ for index in range(len(pages)):
     plt.title("Page {}".format(page))
     
 plt.show()
+# since it is a policy paper, its vocabularies are mainly scientific, but words that implies shared values are also used to emphasize citizen participation.
+
+# NUMBER OF WORDS  
+# filter the document-term matrix to contain only non-zero items
+freq = []
+for pages in dtm.columns:
+    words = dtm[pages].to_numpy().nonzero()[0].size
+    freq.append(words)
+
+wordfreq = pd.Series(freq)
+
+wordfreq.plot(kind="bar", title = "Word Frequency for Malaysia Roadmap towards Zero Single-Use Plastic", 
+              xlabel = "Page", ylabel = "Word Frequency")
