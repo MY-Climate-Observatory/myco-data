11149179ba0b73f829cc6bcea702d90f41a3e1fe
XD-Ooi <43445880+XD-Ooi@users.noreply.github.com>
2020-10-30 15:53:17 +0800

    Update scan-pdf.py

--- CLIMATExGovernment/my-national-policy-2002/scan-pdf.py
+++ CLIMATExGovernment/my-national-policy-2002/scan-pdf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-25 Oct 2020
+30 Oct 2020
 Author: Xiandi Ooi
 
 We will convert the pdf into image page by page. 
@@ -11,6 +11,11 @@ from PIL import Image
 from pdf2image import convert_from_path
 import pytesseract
 import os
+import pandas as pd
+import re
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
 
 def scan_pdf(inputfile):
     # converting the pdf file to image
@@ -24,16 +29,68 @@ def scan_pdf(inputfile):
     # scanning the text using OCR
     # note that the document is a little old so there might be some inaccuracies 
     file = open("dasar_as.txt", "a")
+    pdf_dict = {}
     for i in range(1, counter):
         imagename = "page_" + str(i) + ".jpg"
         text = str(((pytesseract.image_to_string(Image.open(imagename))))) 
         text = text.replace("-\n", "")
-        file.write(text) 
+        file.write(text)
+        pdf_dict[i] = text
+    
+    # finalize the outputs
     file.close()
-    return file
+    
+    # transform the dictionary into pandas dataframe
+    data = pd.DataFrame.from_dict(pdf_dict, orient = "index")
+    data.columns = ["page_content"] 
+    
+    # clean the data
+    data_clean = pd.DataFrame(data.page_content.apply(lambda x:clean(x)))
+    
+    # transforming the corpus into a document-term matrix
+    cv = CountVectorizer(stop_words="english")
+    data_cv = cv.fit_transform(data_clean.page_content)
+    dtm = pd.DataFrame(data_cv.toarray(), columns=cv.get_feature_names())
+    dtm.index = data_clean.index  
+    
+    # save the text objects aside
+    dtm.to_pickle("das-dtm.pkl")
+    data_clean.to_pickle("das-corpus.pkl")
+    pickle.dump(cv, open("das-cv.pkl", "wb"))
+    
+    # export the dataframe 
+    data_clean.to_csv("DAS-corpus.csv")
+    dtm.to_csv("DAS-dtm.csv")
+    
+    return dtm
+
+def clean(text):
+    """
+    text: a string object
+    output: the cleaned string object
+    
+    The function performs basic text cleaning operations including making text
+    lower case, removing non-sensical text, removing puctuation, and
+    removing words containing numbers.
+    """
+    """
+    The data cleaning process is modified from the following source:
+        Title: 1-Data-Cleaning, NLP in Python Tutorial
+        Author: Alice Zhao
+        Date: 2018
+        Availability: https://github.com/adashofdata/nlp-in-python-tutorial/blob/master/1-Data-Cleaning.ipynb
+    """    
+    text = text.lower()
+    text = re.sub("\n", "", text)
+    text = re.sub("\[.*?\]", " ", text)
+    text = re.sub("[%s]" % re.escape(string.punctuation), "", text)
+    text = re.sub("\w*\d\w*", "", text)
+    text = re.sub("[‘’“”…ł]", " ", text)
+    text = re.sub("[˜˚˛˝˙œ™]", "", text)
+    return text
 
 def main():    
-    os.chdir(r"filepath")
+    os.chdir(r"C:\Users\Xiandi\Desktop\Python")
     inputfile = "dasar_as.pdf"
     scan_pdf(inputfile)
 
